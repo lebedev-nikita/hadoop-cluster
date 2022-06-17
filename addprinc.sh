768781e303b3a1f05ac518858c2bfc74b0f03d6a
Nikita Lebedev <y87cgp@gmail.com>
2022-06-17 22:07:16 +0300

    Поправил /etc/hosts

--- addprinc.sh
+++ addprinc.sh
@@ -1,17 +1,17 @@
 service=$1
-echo -e "$service\n$service" | kadmin -w password -q "addprinc $service/host1.consultant.ru@HADOOP.CONSULTANT.RU"
-echo -e "$service\n$service" | kadmin -w password -q "addprinc $service/host2.consultant.ru@HADOOP.CONSULTANT.RU"
-echo -e "$service\n$service" | kadmin -w password -q "addprinc $service/host3.consultant.ru@HADOOP.CONSULTANT.RU"
-echo -e "$service\n$service" | kadmin -w password -q "addprinc $service/host4.consultant.ru@HADOOP.CONSULTANT.RU"
+echo addprinc $service/hadoop-master.consultant.ru@HADOOP.CONSULTANT.RU
+echo addprinc $service/hadoop-slave1.consultant.ru@HADOOP.CONSULTANT.RU
+echo addprinc $service/hadoop-slave2.consultant.ru@HADOOP.CONSULTANT.RU
+echo addprinc $service/hadoop-slave3.consultant.ru@HADOOP.CONSULTANT.RU
 
 # echo -e "airflow\nairflow" | kadmin -w password -q "addprinc airflow/host1.consultant.ru@HADOOP.CONSULTANT.RU"
 
 kadmin:
 
-kvno yarn/host1.consultant.ru@HADOOP.CONSULTANT.RU
-kvno yarn/host2.consultant.ru@HADOOP.CONSULTANT.RU
-kvno yarn/host3.consultant.ru@HADOOP.CONSULTANT.RU
-kvno yarn/host4.consultant.ru@HADOOP.CONSULTANT.RU
+kvno yarn/hadoop-master.consultant.ru@HADOOP.CONSULTANT.RU
+kvno yarn/hadoop-slave1.consultant.ru@HADOOP.CONSULTANT.RU
+kvno yarn/hadoop-slave2.consultant.ru@HADOOP.CONSULTANT.RU
+kvno yarn/hadoop-slave3.consultant.ru@HADOOP.CONSULTANT.RU
 
 kvno hdfs/host1.consultant.ru@HADOOP.CONSULTANT.RU
 kvno yarn/host1.consultant.ru@HADOOP.CONSULTANT.RU
@@ -24,15 +24,15 @@ kvno airflow/host1.consultant.ru@HADOOP.CONSULTANT.RU
 kvno hdfs/host1.consultant.ru@HADOOP.CONSULTANT.RU
 
 sudo ktutil:
-addent -password -p hdfs/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
-addent -password -p yarn/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
-addent -password -p mapred/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
-addent -password -p hbase/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
+addent -password -p hdfs/hadoop-slave1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
+addent -password -p yarn/hadoop-slave1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
+addent -password -p mapred/hadoop-slave1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
+addent -password -p hbase/hadoop-slave1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
 
-addent -password -p HTTP/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
-addent -password -p spark/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 5 -e aes256-cts
-addent -password -p hive/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 6 -e aes256-cts
-# addent -password -p airflow/host1.consultant.ru@HADOOP.CONSULTANT.RU -k 6 -e aes256-cts
+addent -password -p HTTP/hadoop-master.consultant.ru@HADOOP.CONSULTANT.RU -k 1 -e aes256-cts
+addent -password -p spark/hadoop-master.consultant.ru@HADOOP.CONSULTANT.RU -k 5 -e aes256-cts
+addent -password -p hive/hadoop-master.consultant.ru@HADOOP.CONSULTANT.RU -k 6 -e aes256-cts
+# addent -password -p airflow/hadoop-master.consultant.ru@HADOOP.CONSULTANT.RU -k 6 -e aes256-cts
 
 wkt /etc/security/krb5.keytab
 
@@ -40,4 +40,4 @@ sudo chown hadoop:hadoop /etc/security/krb5.keytab
 sudo chmod g+r /etc/security/krb5.keytab
 ll /etc/security/krb5.keytab
 
-kinit spark/host1.consultant.ru@HADOOP.CONSULTANT.RU -kt /etc/security/krb5.keytab
+kinit hdfs/$(hostname -f)@HADOOP.CONSULTANT.RU -kt /etc/security/krb5.keytab
